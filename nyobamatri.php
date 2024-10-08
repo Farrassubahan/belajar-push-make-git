2683df19d7625f9434a8f8f68b5778356ce6b839
Farrassubahan <If23.muhamadsubahan@mhs.ubpkarawang.ac.id>
2024-10-08 23:54:22 +0700

    update file php

--- nyobamatri.php
+++ nyobamatri.php
@@ -12,3 +12,45 @@ $mobil1 ->warna = "hitam";
 $mobil1 ->kecepatan = "1000";
 
 $mobil-> jalan();
+
+
+class mobi {
+    public $warna;
+    public $kecepatan;
+    public function jalan(){
+        echo "mobil lagi ngedrift";
+    }
+}
+$mobil1 = new mobil();
+$mobil1 ->warna = "hitam";
+$mobil1 ->kecepatan = "1000";
+
+$mobil-> jalan();
+
+
+class mobl {
+    public $warna;
+    public $kecepatan;
+    public function jalan(){
+        echo "mobil lagi ngedrift";
+    }
+}
+$mobil1 = new mobil();
+$mobil1 ->warna = "hitam";
+$mobil1 ->kecepatan = "1000";
+
+$mobil-> jalan();
+
+
+class mbil {
+    public $warna;
+    public $kecepatan;
+    public function jalan(){
+        echo "mobil lagi ngedrift";
+    }
+}
+$mobil1 = new mobil();
+$mobil1 ->warna = "hitam";
+$mobil1 ->kecepatan = "1000";
+
+$mobil-> jalan();
